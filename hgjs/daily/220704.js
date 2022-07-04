aa2e194c8cd7881865a26288ab263bf3e6fa96d9
Dev Ryan Kwon <103974022+Dev-RyanK@users.noreply.github.com>
2022-07-05 01:42:48 +0900

    for 문 사용해서 별 배치하기 (정사각, 역삼각, 삼각)

--- hgjs/daily/220704.js
+++ hgjs/daily/220704.js
@@ -1,8 +1,26 @@
 let output = ''
-    for (let a = 1; a<10; a++) { 
-        for (let b = 5; b < a; b++) {
+    for (let a = 1; a<=5; a++) { 
+        for (let b = 1; b<=5; b++) {
             output += '*' //별 생성
         }
         output += '\n' //줄 생성
+    }
+    console.log(output)
+
+let output = ''
+    for (let a = 1; a<=5; a++) {
+        for (let b = 6; b>a; b--) {
+            output += '*'
+        }
+        output += '\n'
+    }
+    console.log(output)
+
+let output = ''
+    for (let a = 1; a<=5; a++) {
+        for (let b = 1; b<=a; b++) {
+            output += '*'
+        }
+        output += '\n'
     }
     console.log(output)

6ac0cf331a47cac09719921febbbf43334bec23d
Dev Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-07-19 15:17:05 +0900

    삼항연산자 다시 풀기

--- hgjs/daily/220719.js
+++ hgjs/daily/220719.js
@@ -1,8 +1,13 @@
 // 삼항연산자
 
-let input = prompt("number")
+let input = prompt("숫자를 입력하세요.")
 let number = Number(input)
-Number(input) % 2 === 0 ? alert("짝수") : alert ("홀수")
+// Number(input) % 2 === 0 ? alert("짝수") : alert ("홀수")
 
-// 홀짝이 아닌 경우에는 어떻게?? (푸는 중)
-Number(input) % 2 === 0 ? alert ("짝수") ? Number(input) % 2 === 1 : alert("홀수") : alert("숫자 아님")
+// 홀짝이 아닌 경우
+// Number(input) % 2 === 0 ? alert ("짝수") : Number(input) % 2 === 1 ? alert("홀수") : alert("숫자 아님")
+// a ? b : (c ? d : e) 이렇게 돌아감
+
+// 홀짝이 아닌 경우2 하는 중
+// a ? (b ? c : d) : e
+Number(input) % 2 === 0 ? Number(input) % 2 === 1 ? alert("홀수") : alert("짝수") : alert("숫자 아님")

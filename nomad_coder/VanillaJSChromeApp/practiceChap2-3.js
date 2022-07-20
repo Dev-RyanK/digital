ad61b764753d02a810130d5eb68f37ef0d9bf8d0
Dev Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-07-20 17:13:10 +0900

    variables, boolean

---
+++ nomad_coder/VanillaJSChromeApp/practiceChap2-3.js
@@ -0,0 +1,13 @@
+const a = 10;
+const b = 2;
+let myName = 'ryan';
+// variables cannot have spaces. Use uppercase instead.(camelCase) (python style: use underbar. my_name. snake_case)
+
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log('hello ' + myName);
+
+myName = 'saul';
+console.log('your new name is ' + myName);

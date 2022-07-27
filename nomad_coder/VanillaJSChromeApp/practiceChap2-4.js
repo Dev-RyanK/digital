62c42c0f6d5c9b43676fe4a837ebeb097599f3ce
Dev Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-07-27 16:29:23 +0900

    Booleans

--- nomad_coder/VanillaJSChromeApp/practiceChap2-4.js
+++ nomad_coder/VanillaJSChromeApp/practiceChap2-4.js
@@ -1,1 +1,11 @@
 // Boolean
+// true or false, 0 or 1
+
+const imNotFat = true;
+const amIFat = null;
+let something;
+console.log(imNotFat); //true
+console.log(something); //undefined
+console.log(amIFat); //null
+
+// true, false, null, undefined = type

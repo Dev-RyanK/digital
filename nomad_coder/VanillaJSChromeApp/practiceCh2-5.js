4b29fcdc33858bc4268d4514d538e2ec8e368823
Dev Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-07-27 16:55:45 +0900

    Arrays: const a = [data]

---
+++ nomad_coder/VanillaJSChromeApp/practiceCh2-5.js
@@ -0,0 +1,24 @@
+// Arrays
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+const daysOfWeek1 = mon + tue + wed + thu + fri + sat + sun // a bad idea, bc/ it cannot pull out certain element of the line
+console.log(daysOfWeek1) //montuewedthufrisatsun
+
+const daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+const nonsense = [1, 2, "hello", false, null, true, undefined, "ryan"];
+
+console.log(daysOfWeek)
+console.log(nonsense);
+
+// get item from array
+console.log(daysOfWeek[5]);
+
+// add more item to the array
+nonsense.push('JS');
+console.log(nonsense);

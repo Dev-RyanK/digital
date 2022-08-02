7df3005add851d3118ad57f579a3082a49ebecfe
Dev Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-08-02 13:24:56 +0900

    1.입출력과 사칙연산 (1)~(3)

---
+++ algorithm/220802-input.js
@@ -0,0 +1,9 @@
+console.log('Hello World!');
+console.log('강한친구 대한육군');
+console.log('강한친구 대한육군');
+
+// 역슬래시는 두 개로 표현
+console.log("\\    /\\");
+console.log(" )  ( ')");
+console.log("(  /  )");
+console.log(" \\(__)|");

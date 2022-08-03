431e971076e61bbce56e62f0dbd98733e2f7bb6a
Dev Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-08-03 14:59:31 +0900

    upload

---
+++ uproject-main/injae2/js/script.js
@@ -0,0 +1,147 @@
+/* global monogatari */
+
+// Define the messages used in the game.
+monogatari.action ('message').messages ({
+	'Help': {
+		title: 'Help',
+		subtitle: 'Some useful Links',
+		body: `
+			<p><a href='https://developers.monogatari.io/documentation/'>Documentation</a> - Everything you need to know.</p>
+			<p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>
+		`
+	}
+});
+
+// Define the notifications used in the game
+monogatari.action ('notification').notifications ({
+	'Welcome': {
+		title: 'Welcome',
+		body: 'This is the Monogatari VN Engine',
+		icon: ''
+	}
+});
+
+// Define the Particles JS Configurations used in the game
+monogatari.action ('particles').particles ({
+
+});
+
+// Define the canvas objects used in the game
+monogatari.action ('canvas').objects ({
+
+});
+
+// Credits of the people involved in the creation of this awesome game
+monogatari.configuration ('credits', {
+
+});
+
+
+// Define the images that will be available on your game's image gallery
+monogatari.assets ('gallery', {
+
+});
+
+// Define the music used in the game.
+monogatari.assets ('music', {
+
+});
+
+// Define the voice files used in the game.
+monogatari.assets ('voices', {
+
+});
+
+// Define the sounds used in the game.
+monogatari.assets ('sounds', {
+
+});
+
+// Define the videos used in the game.
+monogatari.assets ('videos', {
+
+});
+
+// Define the images used in the game.
+monogatari.assets ('images', {
+
+});
+
+// Define the backgrounds for each scene.
+monogatari.assets ('scenes', {
+
+});
+
+
+// Define the Characters
+monogatari.characters ({
+	'j': {
+		name: 'player.name', // 여기 하는 중
+		color: '#5bcaff'
+	}
+});
+
+monogatari.script ({
+	// The game starts here.
+	'Start': [
+		'show scene #f7f6f6 with fadeIn',
+		// 'show notification Welcome',
+		{
+			'Input': {
+				'Text': '인재의 이름은 무엇입니까?',
+				'Validation': function (input) {
+					return input.trim ().length > 0;
+				},
+				'Save': function (input) {
+					this.storage ({
+						player: {
+							name: input
+						}
+					});
+					return true;
+				},
+				'Revert': function () {
+					this.storage ({
+						player: {
+							name: ''
+						}
+					});
+				},
+				'Warning': '이름을 정해주세요.'
+			}
+		},
+		'j 테스트입니다',
+		'{{player.name}} Welcome to Monogatari!',
+		{
+			'Choice': {
+				'Dialog': 'j 선택지 1번입니다?',
+				'Yes': {
+					'Text': 'Yes',
+					'Do': 'jump Yes'
+				},
+				'No': {
+					'Text': 'No',
+					'Do': 'jump No'
+				}
+			}
+		}
+	],
+
+	'Yes': [
+		'j Thats awesome!',
+		'Then you are ready to go ahead and create an amazing Game!',
+		'I can’t wait to see what story you’ll tell!',
+		'end'
+	],
+
+	'No': [
+
+		'y You can do it now.',
+
+		'show message Help',
+
+		'y Go ahead and create an amazing Game!',
+		'y I can’t wait to see what story you’ll tell!',
+		'end'
+	]
+});
